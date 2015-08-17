89137a78b2a585fdb880aaf11d21b7d74ea15b03
= <terencebezman@gmail.com>
2015-08-17 16:09:20 -0400

    Fixed gulp syntax

--- gulpfile.js
+++ gulpfile.js
@@ -25,35 +25,35 @@ gulp.task('dist', function () {
 
     return gulp.src('./public/index.html')
         .pipe(assets)
-	.pipe(gulpif('*.js', uglify())
-	.pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
         .pipe(assets.restore())
         .pipe(useref())
         .pipe(gulp.dest('./public/'));
 });
 
- gulp.task('styles', function () {
-        return gulp.src('./public/css/main.scss')
+gulp.task('styles', function () {
+    return gulp.src('./public/css/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/css/'));
-    });
-
- gulp.task('watch', function () {
-     gulp.watch('./public/css/**/*.scss', ['styles', browserSync.reload]);
-     gulp.watch('./public/css/main.scss', ['styles', browserSync.reload]);
-     gulp.watch('./public/scripts/*.js', [browserSync.reload]);
-     gulp.watch('./public/scripts/**/*', [browserSync.reload]);
-     gulp.watch('./public/index.html', [browserSync.reload]);
-     gulp.watch('./public/views/**/*.html', [browserSync.reload]);
-     gulp.watch('./public/views/*.html', [browserSync.reload]);
-     gulp.watch('./resources/views/**/*', [browserSync.reload]);
-     gulp.watch('./app/**/*', [browserSync.reload]);
 });
 
- gulp.task('no-watch', function () {
+gulp.task('watch', function () {
+    gulp.watch('./public/css/**/*.scss', ['styles', browserSync.reload]);
+    gulp.watch('./public/css/main.scss', ['styles', browserSync.reload]);
+    gulp.watch('./public/scripts/*.js', [browserSync.reload]);
+    gulp.watch('./public/scripts/**/*', [browserSync.reload]);
+    gulp.watch('./public/index.html', [browserSync.reload]);
+    gulp.watch('./public/views/**/*.html', [browserSync.reload]);
+    gulp.watch('./public/views/*.html', [browserSync.reload]);
+    gulp.watch('./resources/views/**/*', [browserSync.reload]);
+    gulp.watch('./app/**/*', [browserSync.reload]);
+});
+
+gulp.task('no-watch', function () {
     gulp.watch('./public/scss/**/*.scss', [browserSync.reload]);
     gulp.watch('./public/scss/main.scss', [browserSync.reload]);
     gulp.watch('./public/scripts/*.js', [browserSync.reload]);
@@ -65,7 +65,7 @@ gulp.task('dist', function () {
     gulp.watch('./resources/views/**/*', [browserSync.reload]);
 });
 
- gulp.task('browser-sync', function () {
+gulp.task('browser-sync', function () {
     connect.server({
         base: './public'
     }, function () {
@@ -77,5 +77,5 @@ gulp.task('dist', function () {
     });
 });
 
- gulp.task('default', ['styles', 'watch', 'browser-sync']);
- gulp.task('no', ['no-watch', 'browser-sync']);
+gulp.task('default', ['styles', 'watch', 'browser-sync']);
+gulp.task('no', ['no-watch', 'browser-sync']);
