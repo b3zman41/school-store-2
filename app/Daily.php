524053c4f248047b714f76084d7e3bd0a70cf6b5
= <terencebezman@gmail.com>
2015-08-18 13:26:51 -0400

    Added number casts on daily

--- app/Daily.php
+++ app/Daily.php
@@ -7,6 +7,13 @@ class Daily extends Model {
     protected $table = "daily";
 	protected $fillable = ["period", "start_cash", "start_check", "end_cash", "end_check", "account_id", "comment"];
 
+    protected $casts  = [
+        'start_cash' =>  'double',
+        'start_check' => 'double',
+        'end_cash' =>  'double',
+        'end_check' => 'double',
+    ];
+
     public function sales()
     {
         return $this->hasMany("App\Sale", "form_id")->with("item");
